60f344cada9521de6309c5b289cfd719f3cac539
LThmly <logancthomley@gmail.com>
2026-03-28 18:49:41 -0400

    Add kalman and new dataset

--- src/Kalman.hpp
+++ src/Kalman.hpp
@@ -1,7 +1,28 @@
 #pragma once
 
+#include <Eigen/Dense>
+#include "State.hpp"
+
 namespace Kalman {
 
-class Filter {};
+class Filter {
+    public:
+        // constructor
+        Filter(double odom_x, double odom_y);
 
-}  // namespace Kalman
+        // main steps of kalman: Predicting and then Updating
+        void predict(double prior_odom_x, double prior_odom_y, double odom_x,
+                     double odom_y, double imu_heading);
+
+        void update(double imu_heading, double front_sens, double left_sens,
+                    double right_sens);
+
+        Pose getEstimation() const;
+        
+    private:
+        Eigen::Vector2d state;
+        Eigen::Matrix2d cov; // covariance
+        Eigen::Matrix2d noise;
+        double heading = 0.0; // current heading
+};
+}
